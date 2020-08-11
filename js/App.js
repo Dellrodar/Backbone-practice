bf67ce98dd505b837fd674351a46c76993928f12
Emilio Crocco <emilio.crocco@C02NJ4E7G3QN.local>
2020-08-11 15:46:35 -0400

    Modularization * Songs collection modularized and connected to App * Moved songs functions into initalize function

--- js/App.js
+++ js/App.js
@@ -2,8 +2,9 @@ define([
   'underscore',
   'backbone',
   'models/song',
-  'views/songView'
-], function (_, Backbone, Song, SongView) {
+  'views/songView',
+  'collections/songs'
+], function (_, Backbone, Song, SongView, Songs) {
 
   var initialize = function () {
     var song = new Song({
@@ -17,42 +18,49 @@ define([
     var songView = new SongView({ el: "#song", model: song });
     songView.render();
 
-  }
+    // New Songs to add to our collection
+    var songs = new Songs([
+      new Song({ id: 1, title: "Drowning in my code" }),
+      new Song({ id: 2, title: "Coffee is love and life" }),
+      new Song({ id: 3, title: "Java made the code go down" })
+    ]);
+
+    // Songs can be added using the ".add()" function. Each new song will require an title based on our validation.
+    songs.add(new Song({ id: 4, title: "This is only a test." }));
+    songs.add(new Song({ id: 5, title: "Gaining a backbone", genre: "Rock", downloads: 9001 }), { at: 0 });
+
+    // ".push" can also be used to add songs.
+    songs.push(new Song({ id: 6, title: "Gaining the knowledge...", genre: "Classical", downloads: 100 }));
+
+    // ".where" can be used to find all songs with the genre of Jazz on the page.
+    var jazzSongs = songs.where({ genre: "Jazz" });
+
+    // ".findWhere" will find the first item the matches the genre
+    var firstJazzSong = songs.findWhere({ genre: "Jazz" });
+
+    // returning each function
+    console.log("Jazz Songs", jazzSongs);
+    console.log("First Jazz Song", firstJazzSong);
+
+    var filteredSongs = songs.where({ genre: "Jazz", title: "Drowning in my code" });
+    console.log("Filtered Songs", filteredSongs);
+
+    var topDownloads = songs.filter(function (song) {
+      return song.get("downloads") > 100;
+    })
+
+    console.log("Top Downloads", topDownloads);
+
+    songs.each(function (song) {
+      console.log(song);
+    });
+
+  };
 
   return {
     initialize: initialize
   };
 
-})
-
-// Songs can be added using the ".add()" function. Each new song will require an title based on our validation.
-songs.add(new Song({ id: 4, title: "This is only a test." }));
-songs.add(new Song({ id: 5, title: "Gaining a backbone", genre: "Rock", downloads: 9001 }), { at: 0 });
-
-// ".push" can also be used to add songs.
-songs.push(new Song({ id: 6, title: "Gaining the knowledge...", genre: "Classical", downloads: 100 }));
-
-// ".where" can be used to find all songs with the genre of Jazz on the page.
-var jazzSongs = songs.where({ genre: "Jazz" });
-
-// ".findWhere" will find the first item the matches the genre
-var firstJazzSong = songs.findWhere({ genre: "Jazz" });
-
-// returning each function
-console.log("Jazz Songs", jazzSongs);
-console.log("First Jazz Song", firstJazzSong);
-
-var filteredSongs = songs.where({ genre: "Jazz", title: "Drowning in my code" });
-console.log("Filtered Songs", filteredSongs);
-
-var topDownloads = songs.filter(function (song) {
-  return song.get("downloads") > 100;
-})
-
-console.log("Top Downloads", topDownloads);
-
-songs.each(function (song) {
-  console.log(song);
 })
 
 var songsView = new SongsView({ el: "#songs", model: songs });
--- js/collections/songs.js
+++ js/collections/songs.js
@@ -1,11 +1,13 @@
-// Our first Collection. A Collection is a group of Models. It is extended from Backbone.Collection
-var Songs = Backbone.Collection.extend({
-  model: Song
-});
+define([
+  'underscore',
+  'backbone',
+  'models/song'],
+  function (_, Backbone, Song) {
+    // Our first Collection. A Collection is a group of Models. It is extended from Backbone.Collection
+    var Songs = Backbone.Collection.extend({
+      model: Song
+    });
+    return Songs;
+  })
 
-// New Songs to add to our collection
-var songs = new Songs([
-  new Song({ id: 1, title: "Drowning in my code" }),
-  new Song({ id: 2, title: "Coffee is love and life" }),
-  new Song({ id: 3, title: "Java made the code go down" })
-]);
+
